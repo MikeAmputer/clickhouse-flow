0f373be94f6d2e02a59713b565f47a17a318656d
Pavel Kravtsov <kravtsovpo@gmail.com>
2024-10-17 01:26:13 +0700

    Get rid of cell blinking borders and add expand button

--- app/components/ChColumn.tsx
+++ app/components/ChColumn.tsx
@@ -5,27 +5,61 @@ import {
 } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
-const NameTableCell = styled(TableCell)(({ theme }) => ({
+const OddNameTableCell = styled(TableCell)(({ theme }) => ({
     fontSize: 14,
     color: '#000',
     textAlign: 'left',
     padding: '5px',
+    backgroundColor: '#fffbd4',
+    borderRight: 'solid',
+    borderRightWidth: 2,
+    borderColor: '#fffbd4',
 }));
 
-const TypeTableCell = styled(TableCell)(({ theme }) => ({
+const EvenNameTableCell = styled(TableCell)(({ theme }) => ({
+    fontSize: 14,
+    color: '#000',
+    textAlign: 'left',
+    padding: '5px',
+    backgroundColor: '#ebebeb',
+    borderRight: 'solid',
+    borderRightWidth: 2,
+    borderColor: '#ebebeb',
+}));
+
+const OddTypeTableCell = styled(TableCell)(({ theme }) => ({
     fontSize: 12,
     color: '#888',
     textAlign: 'right',
     padding: '5px',
+    borderLeft: 'solid',
+    borderLeftWidth: 2,
+    borderColor: '#fffbd4',
+    backgroundColor: '#fffbd4',
+}));
+
+const EvenTypeTableCell = styled(TableCell)(({ theme }) => ({
+    fontSize: 12,
+    color: '#888',
+    textAlign: 'right',
+    padding: '5px',
+    borderLeft: 'solid',
+    borderLeftWidth: 2,
+    borderColor: '#ebebeb',
+    backgroundColor: '#ebebeb',
 }));
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
     '&:nth-of-type(even)': {
         backgroundColor: '#ebebeb',
+        borderColor: '#ebebeb',
     },
     '&:nth-of-type(odd)': {
         backgroundColor: '#fffbd4',
+        borderColor: '#fffbd4',
     },
+    borderBottom: 'solid',
+    borderBottomWidth: 2,
 }));
 
 export type ChColumnProps = {
@@ -37,10 +71,14 @@ export type ChColumnProps = {
 }
 
 const ChColumn: React.FC<ChColumnProps> = (column) => {
+    const isOdd = column.position % 2 === 1;
+    const NameCell = isOdd ? OddNameTableCell : EvenNameTableCell;
+    const TypeCell = isOdd ? OddTypeTableCell : EvenTypeTableCell;
+
     return (
         <StyledTableRow>
-            <NameTableCell>{column.name}</NameTableCell>
-            <TypeTableCell>{column.type}</TypeTableCell>
+            <NameCell>{column.name}</NameCell>
+            <TypeCell>{column.type}</TypeCell>
         </StyledTableRow>
     );
 };
--- app/components/ChTable.tsx
+++ app/components/ChTable.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import {
     Table,
     TableBody,
@@ -7,16 +8,48 @@ import {
     TableHead,
     TableRow,
     Paper,
+    IconButton,
 } from '@mui/material';
+
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+
 import { styled } from '@mui/material/styles';
+
 import ChColumn, { ChColumnProps } from './ChColumn';
 
-const StyledTableHeadCell = styled(TableCell)(({ theme }) => ({
+const StyledTable = styled(Table)(({ theme }) => ({
+    borderCollapse: "collapse",
+    borderSpacing: 0,
+    background: "#000",
+}));
+
+const TableNameCell = styled(TableCell)(({ theme }) => ({
     backgroundColor: '#000',
     color: '#fff',
     fontSize: 14,
     textAlign: 'left',
     padding: '5px',
+}));
+
+const ExpandCell = styled(TableCell)(({ theme }) => ({
+    backgroundColor: '#000',
+    textAlign: 'right',
+    padding: '5px',
+}));
+
+const ExpandButton = styled(IconButton)(({ theme }) => ({
+    color: '#fff',
+    '&:hover': {
+        backgroundColor: '#383838',
+        color: '#fff',
+    },
+}));
+
+const TableHeadRow = styled(TableRow)(({ theme }) => ({
+    borderBottom: 'solid',
+    borderBottomWidth: 0,
+    borderColor: '#000',
 }));
 
 export type ChTableProps = {
@@ -25,22 +58,31 @@ export type ChTableProps = {
 }
 
 const ChTable: React.FC<ChTableProps> = (table) => {
+    const [open, setOpen] = useState(true);
+
     return (
         <TableContainer component={Paper}>
-            <Table size={'small'}>
+            <StyledTable size={'small'}>
                 <TableHead>
-                    <TableRow>
-                        <StyledTableHeadCell align="center" colSpan={2}>
-                            {table.name}
-                        </StyledTableHeadCell>
-                    </TableRow>
+                    <TableHeadRow>
+                        <TableNameCell>{table.name}</TableNameCell>
+                        <ExpandCell>
+                            <ExpandButton
+                                aria-label={'expand table'}
+                                size={'small'}
+                                onClick={() => setOpen(!open)}
+                            >
+                                {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                            </ExpandButton>
+                        </ExpandCell>
+                    </TableHeadRow>
                 </TableHead>
                 <TableBody>
                     {table.columns.map((column) => (
                         <ChColumn key={`${table.name}_${column.position}`} {...column} />
                     ))}
                 </TableBody>
-            </Table>
+            </StyledTable>
         </TableContainer>
     );
 };
