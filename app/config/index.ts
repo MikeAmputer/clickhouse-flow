267038353cb6cb158bbe70a39d86d9cffc164ca9
Pavel Kravtsov <kravtsovpo@gmail.com>
2025-05-08 01:01:01 +0700

    Fix db configs setup

--- app/config/index.ts
+++ app/config/index.ts
@@ -33,6 +33,22 @@ const defaultConfig: ConfigFile =
   }
 };
 
+function loadConfigFile(): ConfigFile | null {
+  const configPath: string = ENV.NODE_ENV !== 'production'
+    ? path.resolve('./app/config/dev-config.json')
+    : ENV.CHF_CONFIG_PATH;
+
+  try {
+    if (fs.existsSync(configPath)) {
+      const raw = fs.readFileSync(configPath, 'utf-8').trim();
+      return raw ? (JSON.parse(raw) as ConfigFile) : null;
+    }
+  } catch (err) {
+    console.error(`Error loading config from ${configPath}:`, err);
+  }
+  return null;
+}
+
 function getDefaultDatabaseConfig(): { dbConfig: DatabaseConfig, configName: string } | null {
   const {
     CHF_DEFAULT_DB_URL: url,
@@ -54,20 +70,19 @@ function getDefaultDatabaseConfig(): { dbConfig: DatabaseConfig, configName: str
   return null;
 }
 
-function loadConfigFile(): ConfigFile | null {
-  const configPath: string = ENV.NODE_ENV !== 'production'
-    ? path.resolve('./app/config/dev-config.json')
-    : ENV.CHF_CONFIG_PATH;
+function setupDbConfigs(config: ConfigFile) {
+  config.databaseConfigs ??= [];
 
-  try {
-    if (fs.existsSync(configPath)) {
-      const raw = fs.readFileSync(configPath, 'utf-8').trim();
-      return raw ? (JSON.parse(raw) as ConfigFile) : null;
-    }
-  } catch (err) {
-    console.error(`Error loading config from ${configPath}:`, err);
+  const defaultDbConfig = getDefaultDatabaseConfig();
+
+  if (defaultDbConfig) {
+    const defaultEntry: DatabaseConfigEntry = {
+      name: defaultDbConfig.configName,
+      config: defaultDbConfig.dbConfig,
+    };
+
+    config.databaseConfigs.unshift(defaultEntry);
   }
-  return null;
 }
 
 function substituteEnv(config: ConfigFile) {
@@ -86,17 +101,7 @@ export function getConfig(): ConfigFile {
 
   const config = loadConfigFile() ?? structuredClone(defaultConfig);
 
-  const defaultDbConfig = getDefaultDatabaseConfig();
-
-  if (defaultDbConfig) {
-    const defaultEntry: DatabaseConfigEntry = {
-      name: defaultDbConfig.configName,
-      config: defaultDbConfig.dbConfig,
-    };
-
-    config.databaseConfigs.unshift(defaultEntry);
-  }
-
+  setupDbConfigs(config);
   substituteEnv(config);
 
   if (ENV.CHF_CACHE_CONFIG) {
