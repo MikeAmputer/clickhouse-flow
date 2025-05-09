64275484cf5e7ed5d1d26297f8c3704100bf7df3
Pavel Kravtsov <kravtsovpo@gmail.com>
2025-05-10 02:23:41 +0700

    Allow origin localhost:3000

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        'localhost:3000'
+      ]
+    }
+  }
 };
 
 export default nextConfig;
